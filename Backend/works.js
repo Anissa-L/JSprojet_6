353fee1c97e41787ef51d70089c75c836c193114
Lamtalsi Anissa <lamtalsi.03@gmail.com>
2023-06-19 16:03:21 +0200

    test fetch avec inspiration slack

--- Backend/works.js
+++
@@ -1,2 +0,0 @@
-const works = fetch("http://localhost:5678/api/works");
-console.log(works);
---
+++ FrontEnd/assets/works.js
@@ -0,0 +1,26 @@
+const gallery = document.querySelector(".gallery")
+
+function ajoutPortfolio (data){
+
+    for (let i=0; i<data.length; i++){
+        const card = document.createElement("figure");
+        card.setAttribute("id",data[i].category.name);
+        const image = document.createElement("img");
+        image.src = data[i].image.Url;
+        const imageTitle = document.createElement("figcaption");
+        imageTitle.innerHTML = data[i].title;
+
+        gallery.appendChild(card);
+        card.appendChild(image);
+        image.appendChild(imageTitle);
+    }
+}
+
+fetch("http://localhost:5678/api/works");
+.then(reponse => reponse.json());
+
+.then(data =>{
+    console.log(data);
+
+    ajoutPortfolio(data);
+})
