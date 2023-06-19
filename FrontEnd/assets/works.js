1e9499ec5116cc8626ad5f559c77505b5c1d3c78
Lamtalsi Anissa <lamtalsi.03@gmail.com>
2023-06-19 16:05:48 +0200

    changement then

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -1,26 +1,25 @@
-const gallery = document.querySelector(".gallery")
+const gallery = document.querySelector(".gallery");
 
-function ajoutPortfolio (data){
+function ajoutPortfolio(data) {
+  for (let i = 0; i < data.length; i++) {
+    const card = document.createElement("figure");
+    card.setAttribute("id", data[i].category.name);
+    const image = document.createElement("img");
+    image.src = data[i].image.Url;
+    const imageTitle = document.createElement("figcaption");
+    imageTitle.innerHTML = data[i].title;
 
-    for (let i=0; i<data.length; i++){
-        const card = document.createElement("figure");
-        card.setAttribute("id",data[i].category.name);
-        const image = document.createElement("img");
-        image.src = data[i].image.Url;
-        const imageTitle = document.createElement("figcaption");
-        imageTitle.innerHTML = data[i].title;
-
-        gallery.appendChild(card);
-        card.appendChild(image);
-        image.appendChild(imageTitle);
-    }
+    gallery.appendChild(card);
+    card.appendChild(image);
+    image.appendChild(imageTitle);
+  }
 }
 
 fetch("http://localhost:5678/api/works");
-.then(reponse => reponse.json());
+then((reponse) => reponse.json());
 
-.then(data =>{
-    console.log(data);
+then((data) => {
+  console.log(data);
 
-    ajoutPortfolio(data);
-})
+  ajoutPortfolio(data);
+});
