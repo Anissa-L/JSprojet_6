93276bbee7ef67b608f81ecb694581b8daab154e
Lamtalsi Anissa <lamtalsi.03@gmail.com>
2023-06-22 13:35:51 +0200

    fetch works fini

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -2,24 +2,32 @@ const gallery = document.querySelector(".gallery");
 
 function ajoutPortfolio(data) {
   for (let i = 0; i < data.length; i++) {
-    const card = document.createElement("figure");
-    card.setAttribute("id", data[i].category.name);
-    const image = document.createElement("img");
-    image.src = data[i].image.Url;
-    const imageTitle = document.createElement("figcaption");
-    imageTitle.innerHTML = data[i].title;
+    const figure = document.createElement("figure");
+    figure.setAttribute("id", data[i].category.name);
+    const img = document.createElement("img");
+    img.src = data[i].imageUrl;
+    const figcaption = document.createElement("figcaption");
+    figcaption.innerHTML = data[i].title;
 
-    gallery.appendChild(card);
-    card.appendChild(image);
-    image.appendChild(imageTitle);
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
   }
 }
 
 fetch("http://localhost:5678/api/works")
-  .then((reponse) => reponse.json())
+  .then((reponse) => {
+    console.log("premier .then");
+    console.log(reponse);
+    return reponse.json();
+  })
+
+  //.then(reponse => reponse.json())
 
   .then((data) => {
     console.log(data);
 
     ajoutPortfolio(data);
-  });
+  })
+
+  .catch((error) => console.log(`Erreur : ${error}`));
