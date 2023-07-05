7ecbef8b9bfbdfbb3e64dd51c22f918be3330c1b
Lamtalsi Anissa <lamtalsi.03@gmail.com>
2023-07-05 21:37:20 +0200

    recherche d'un filtre qui fonctionne

--- FrontEnd/assets/categories.js
+++ FrontEnd/assets/categories.js
@@ -1,43 +1,85 @@
 //constante boutton category
-const categorieObjets = document.querySelector(".categorieObjets");
+/*const categorieObjets = document.querySelector(".categorieObjets");
 
 const categorieAppartements = document.querySelector(".categorieAppartements");
 
-const categorieHR = document.querySelector(".catergorieHotelsAndRestaurants");
+const categorieHR = document.querySelector(".catergorieHotelsAndRestaurants");*/
 
-//const categorieTout = document.querySelector(".categorieNone");
+const boutton_categories = document.querySelector(".boutton_categories");
 
-//fonction aucun filtre
-//categorieNone.addEventListener("click", function () {
-//const NoneFilter = None.filter(function (category) {
-//return (category.userId = 1);
-// });
-//console.log(objetFilter);
-//});
+// Fonction pour créer un bouton dans la nav des filtres
+function ajoutCategories(category) {
+  const buttonFilters = document.createElement("button");
+  buttonFilters.setAttribute("data-tag", category.name);
+  buttonFilters.setAttribute("data-id", category.id);
+  buttonFilters.innerText = category.name;
+  //ajout de classes
+  if (category.id === 1) {
+    buttonFilters.classList.add("objets");
+  }
+  if (category.id === 2) {
+    buttonFilters.classList.add("appartements");
+  }
+  if (category.id === 3) {
+    buttonFilters.classList.add("hotelsEtRestaurants");
+  }
+  boutton_categories.appendChild(buttonFilters);
 
-//fonction filtre objets
-categorieObjets.addEventListener("click", function () {
-  const objetFilter = Objets.filter(function (category) {
-    return category.id == 1;
+  buttonFilters.addEventListener("click", function () {
+    filtrerParCategorie(category.id);
   });
-  console.log(objetFilter);
-});
+}
 
-//fonction filtre appartements
-categorieAppartements.addEventListener("click", function () {
-  const AppartementsFilter = Appartements.filter(function (category) {
-    return category.id == 2;
-  });
-  console.log(AppartementsFilter);
-});
+//création du boutton tous
+const bouttonTous = document.createElement("button");
+bouttonTous.className = "tous";
+bouttonTous.innerText = "Tous";
+boutton_categories.appendChild(bouttonTous);
 
-//fonction filtre hotels & restaurants
-categorieHR.addEventListener("click", function () {
-  const categorieHRFilter = categorieHR.filter(function (category) {
-    return category.id == 3;
+const boutton_objets = document.querySelector(".objets");
+const boutton_appartements = document.querySelector(".appartements");
+const boutton_hotelsEtRestaurants = document.querySelector(
+  ".hotelsEtRestaurants"
+);
+
+function filtrerParCategorie(categorieId) {
+  // Code pour filtrer les données en fonction de l'identifiant de catégorie
+  // ...
+  console.log("Filtrage par catégorie avec l'ID :", categorieId);
+
+  const figures = document.querySelectorAll(".gallery figure");
+
+  figures.forEach((figure) => {
+    const tag = figure.getAttribute("id");
+    /*if (tag === "Objets") {
+      figure.style.display = "block";
+    } else {
+      figure.style.display = "none";
+    }
+
+    if (tag === "Appartements") {
+      figure.style.display = "block";
+    } else {
+      figure.style.display = "none";
+    }
+
+    if (tag === "Hotels & Restaurants") {
+      figure.style.display = "block";
+    } else {
+      figure.style.display = "none";
+    }*/
+
+    if (
+      tag === "Objets" ||
+      tag === "Appartements" ||
+      tag === "Hotels & Restaurants"
+    ) {
+      figure.style.display = "block";
+    } else {
+      figure.style.display = "none";
+    }
   });
-  console.log(categorieHRFilter);
-});
+}
 
 // fetch category
 fetch("http://localhost:5678/api/categories")
@@ -49,10 +91,14 @@ fetch("http://localhost:5678/api/categories")
 
   //.then(reponse => reponse.json())
 
-  .then((data) => {
-    console.log(data);
+  .then((categories) => {
+    console.log(categories);
 
-    ajoutPortfolio(data);
+    //Auxquelles on applique la fonction createButton
+    categories.forEach((categorie) => {
+      console.log(categorie);
+      ajoutCategories(categorie);
+    });
   })
 
   .catch((error) => console.log(`Erreur : ${error}`));
