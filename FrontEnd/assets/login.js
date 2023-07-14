c5c60a9e86e0c4d30e5c335082c16e99117c66d0
Lamtalsi Anissa <lamtalsi.03@gmail.com>
2023-07-14 23:47:04 +0200

    modif small login et fetch login et ajout html->js

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,12 +1,7 @@
-async function main() {
-  await postLogin();
-}
-main();
-
 async function postLogin() {
   let user = {
-    email: "sophie.bluel@test.tld",
-    password: "S0phie",
+    eMail: "sophie.bluel@test.tld",
+    passWord: "S0phie ",
   };
 
   let response = await fetch("http://localhost:5678/api/users/login", {
@@ -43,33 +38,46 @@ const setError = (element, message) => {
   small.innerText = message;
 
   //Ajout de la classe error Suppression de la classe success
-  formControl.classList.add(".error");
-  formControl.classList.remove(".success");
+  formControl.classList.add("error");
+  formControl.classList.remove("success");
 };
 
+/*
 //Success
 const setSuccess = (element) => {
   const formControl = element.parentElement;
-  const errorDisplay = formControl.querySelector(".error");
 
-  errorDisplay.innerText = "";
-  formControl.classList.add(".success");
-  formControl.classList.remove(".error");
-};
+  formControl.classList.add("success");
+  formControl.classList.remove("error");
+};*/
 
+/*
 //Paramètre de l'email
 const isValidEmail = (email) => {
   const re =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-] +@ [a-zA-Z0 -9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   return re.test(String(email).toLowerCase());
-};
+};*/
 
 //Input Vérification
 const validateInputs = () => {
+  let eMail = "sophie.bluel@test.tld";
+  let passWord = "S0phie";
+
   //Obtenir les valeurs des inputs
   const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
 
+  if (emailValue != eMail || passwordValue != passWord) {
+    let message = "Erreur dans l’identifiant ou le mot de passe";
+    setError(password, message);
+    console.log(setError);
+  } else {
+    document.location.href = "index.html";
+    let edit = document.createElement("div");
+    edit.className = "edit";
+  }
+  /*
   //vérification email
   if (emailValue === "") {
     let message = "Une saisie email est requise";
@@ -85,10 +93,12 @@ const validateInputs = () => {
 
   //vérification mot de passe
   if (passwordValue === "") {
-    setError(password, "Mot de passe incorrect");
+    let message = "Mot de passe incorrect";
+    setError(password, message);
   } else if (passwordValue.length < 5) {
-    setError(password, "Le mot de passe doit contenir au moins 5 caractère.");
+    let message = "Le mot de passe doit contenir au moins 5 caractère.";
+    setError(password, message);
   } else {
     setSuccess(password);
-  }
+  }*/
 };
